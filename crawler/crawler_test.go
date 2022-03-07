86a0d7b32899b838e27e5c23365aeea596b889b4
Amal Aruja <amalaruja@Amal-Arujas-Personal-MacBook-Pro.local>
2022-03-07 16:56:23 +0530

    [httptest] Uses httptest to wite a more robust test on the crawler

--- crawler/crawler_test.go
+++ crawler/crawler_test.go
@@ -1,24 +1,58 @@
 package crawler_test
 
 import (
+	"encoding/json"
 	"fmt"
 	"golang-training/crawler"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
 func TestBillFetch(t *testing.T) {
-	url := "https://mocki.io/v1/82f434ce-4124-45a2-901c-374ca88a4429"
+	t.Run("GiveServerURLWhenFetchIsCalledShouldReturnBill", func(t *testing.T) {
+		url := "https://mocki.io/v1/82f434ce-4124-45a2-901c-374ca88a4429"
 
-	crwler := crawler.NewCrawler(url)
+		crwler := crawler.NewCrawler(url)
 
-	userID := 1234
-	bill, err := crwler.FetchBill(userID)
+		userID := 1234
+		bill, err := crwler.FetchBill(userID)
 
-	assert.Nil(t, err)
-	assert.NotNil(t, bill)
-	assert.Equal(t, userID, bill.ID)
+		assert.Nil(t, err)
+		assert.NotNil(t, bill)
+		assert.Equal(t, userID, bill.ID)
 
-	fmt.Println(bill)
+		fmt.Println(bill)
+	})
+
+	t.Run("GivenServerRespondsWithStatusOKAndBillThenShouldReturnBillWithoutError", func(t *testing.T) {
+		server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			bill := crawler.Bill{
+				ID:     1234,
+				Name:   "Amal",
+				Month:  "Feb",
+				Amount: 1000,
+			}
+			bytes, err := json.Marshal(bill)
+
+			assert.Nil(t, err)
+
+			w.Write(bytes)
+		}))
+
+		defer server.Close()
+
+		crwler := crawler.NewCrawler(server.URL)
+
+		userID := 1234
+		bill, err := crwler.FetchBill(userID)
+
+		assert.Nil(t, err)
+		assert.NotNil(t, bill)
+		assert.Equal(t, userID, bill.ID)
+
+		fmt.Println(bill)
+	})
 }
